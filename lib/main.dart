54fc0b39af3554ae5a8035ce133c4d5681fd10b6
samuchakraborty <samuckb@gmail.com>
2020-11-19 17:33:12 +0600

    Pixel problem slove

--- lib/main.dart
+++ lib/main.dart
@@ -62,98 +62,121 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      //body: Text("hello"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        //body: Text("hello"),
 
-      body: Container(
-        child: FutureBuilder(
-            future: loadPhoto(),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.data == null) {
-                return Container(
-                  child: Center(
-                    child: Text("Loading"),
-                  ),
-                );
-              } else {
-                return GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                                          child: Column(
-                        children: [
-                          Expanded(
-                            flex: 1,
-                            child: Container(
-                              alignment: Alignment.topLeft,
-                              padding: EdgeInsets.all(5),
-                              child: Text(
-                                "Sale",
-                                style: TextStyle(backgroundColor: Colors.green),
+        body: Container(
+          child: FutureBuilder(
+              future: loadPhoto(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.data == null) {
+                  return Container(
+                    child: Center(
+                      child: Text("Loading"),
+                    ),
+                  );
+                } else {
+                  return GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        child: Stack(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: MediaQuery.of(context).size.height,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    alignment: Alignment.topLeft,
+                                    height: 15,
+                                    child: Text(
+                                      "Sale",
+                                      style: TextStyle(
+                                        backgroundColor: Colors.green,
+                                      ),
+                                    ),
+                                  ),
+
+                                  Container(
+                                    child: Column(
+                                      children: [
+                                        Image.network(
+                                          "https://test.anazbd.com/" +
+                                              snapshot.data[index].featureImage,
+                                          fit: BoxFit.contain,
+                                          height: 100,
+                                          width: 150,
+                                        ),
+                                        Text(
+                                          snapshot.data[index].name,
+                                          style: TextStyle(
+                                              fontSize: 10,
+                                              fontWeight: FontWeight.w400),
+                                        ),
+                                        // Text("TK: " +
+                                        //     (snapshot.data[index].originalPrice)
+                                        //         .toString()),
+                                      ],
+                                    ),
+
+                                    // leading: Image.network("https://test.anazbd.com/" +
+                                    //     snapshot.data[index].featureImage),
+                                  ),
+
+
+                                  Container(
+                                    alignment: Alignment.topLeft,
+                                   child: Text("TK: " +
+                                              (snapshot.data[index].originalPrice)
+                                                  .toString()),
+                                 ),
+
+                                  // Container(
+                                  //   alignment: Alignment.center,
+                                  //   height: 10,
+                                  //   padding: EdgeInsets.all(1),
+                                  //   child: Text(
+                                  //     snapshot.data[index].name,
+                                  //     style: TextStyle(fontSize: 10),
+                                  //   ),
+                                  // ),
+                                  // Container(
+                                  //   alignment: Alignment.center,
+                                  //   padding: EdgeInsets.all(5),
+                                  //   child: Text("TK: " +
+                                  //       (snapshot.data[index].originalPrice)
+                                  //           .toString()),
+                                  // ),
+
+                                  // Expanded(
+                                  //   flex: 1,
+                                  //   child: Container(
+                                  //     alignment: Alignment.bottomRight,
+                                  //     padding: EdgeInsets.all(6),
+                                  //     child:  IconButton(
+                                  //     onPressed: () {},
+                                  //     icon: Icon(EvaIcons.plus)),
+                                  //   ),
+                                  // ),
+                                ],
+                                 
                               ),
                             ),
-                          ),
-                          Material(
-                            elevation: 3.0,
-                            child: Image.network(
-                              "https://test.anazbd.com/" +
-                                  snapshot.data[index].featureImage,
-                              fit: BoxFit.fitWidth,
-                              height: 40,
-                              width: 100,
-                            ),
-
-                            // leading: Image.network("https://test.anazbd.com/" +
-                            //     snapshot.data[index].featureImage),
-                          ),
-                          Expanded(
-                            flex: 1,
-                            child: Container(
-                              alignment: Alignment.center,
-                              padding: EdgeInsets.all(1),
-                              child: Text(
-                                snapshot.data[index].name,
-                                style: TextStyle(fontSize: 10),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 1,
-                            child: Container(
-                              alignment: Alignment.center,
-                              padding: EdgeInsets.all(5),
-                              child: Text("TK: " +
-                                  (snapshot.data[index].originalPrice)
-                                      .toString()),
-                            ),
-                          ),
-
-
-                          // Expanded(
-                          //   flex: 1,
-                          //   child: Container(
-                          //     alignment: Alignment.bottomRight,
-                          //     padding: EdgeInsets.all(6),
-                          //     child:  IconButton(
-                          //     onPressed: () {},
-                          //     icon: Icon(EvaIcons.plus)),
-                          //   ),
-                          // ),
-
-
-                         
-                        ],
-                      ),
-                    );
-                  },
-                );
-              }
-            }),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }
+              }),
+        ),
       ),
     );
   }
